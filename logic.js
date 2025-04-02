99eee52e60aef946c5c4d12b3b40396dbceb205c
Noeray0 <noe.raymond@stud.kuezh.ch>
2025-04-02 16:26:58 +0200

    formatting changes

--- logic.js
+++ logic.js
@@ -6,7 +6,7 @@ navigator.geolocation.getCurrentPosition(position => {
 
   // Function that handles the weather-based decision logic
 function makeWeatherChoices(maxTemperature, minTemperature, maxUVIndex, precipitationChance, windspeed, percipitationsum, cloudcover) {
-  let message = `Max Temp: ${maxTemperature}°C, Min Temp: ${minTemperature}°C <br> Max UV Index: ${maxUVIndex} <br> Precipitation Chance: ${precipitationChance}%, Percipitation: ${percipitationsum}mm <br> Max Wind Speed: ${windspeed}km/h <br> Cloud Cover: ${cloudcover}%`;
+  let message = `According to todays conditions the following is recomended: <br>`;
 
   // Make decisions based on weather conditions
   if (maxTemperature > 20) {
@@ -49,6 +49,8 @@ function makeWeatherChoices(maxTemperature, minTemperature, maxUVIndex, precipit
   if (cloudcover < 30){
     message += `<br> It could get quite bright today. Bring some sunglasses or a cap.`
   }
+
+  message += `<br><br> Max Temp: ${maxTemperature}°C, Min Temp: ${minTemperature}°C <br> Max UV Index: ${maxUVIndex} <br> Precipitation Chance: ${precipitationChance}%, Percipitation: ${percipitationsum}mm <br> Max Wind Speed: ${windspeed}km/h <br> Cloud Cover: ${cloudcover}%`
 
   // Display the message on the page
   show.innerHTML = message;
