cdf28616909ef6a44cb11f5d6e77e55664d364ae
Noeray0 <noe.raymond@stud.kuezh.ch>
2025-05-06 17:56:56 +0200

    adding images because why not

--- logic.js
+++ logic.js
@@ -1,5 +1,6 @@
 const day_1 = document.getElementById("day_1");
 const week = document.getElementById("week");
+const images = document.getElementById("images");
 
 let selectedLatitude = null;
 let selectedLongitude = null;
@@ -17,7 +18,8 @@ window.addEventListener('load', () => {
 
 // Weekly recommendation logic
 function makeWeeklyRecommendations(maxTemp, minTemp, maxUV, maxPrecipChance, maxWind, totalPrecipSum, minCloudCover) {
-  let message = `Based on the most extreme conditions forecasted this week, here’s what to prepare for:<br>`;
+  let message = `<strong>Next Week</strong><br>`;
+  message += `Based on the most extreme conditions forecasted this week, here’s what to prepare for:<br>`;
 
   if (maxTemp > 40) {
     message += `<br>It could get *very* hot this week. Plan accordingly.`;
@@ -51,44 +53,75 @@ function makeWeeklyRecommendations(maxTemp, minTemp, maxUV, maxPrecipChance, max
 
   if (minCloudCover < 30) {
     message += `<br>There will be some bright days. Sunglasses or a cap might help.`;
-  }
-
+  }  
+  
   message += `<br><br><strong>Weekly Extremes:</strong><br>`;
   message += `Max Temp: ${maxTemp}°C, Min Temp: ${minTemp}°C<br>`;
   message += `Max UV Index: ${maxUV}<br>`;
   message += `Max Precipitation Chance: ${maxPrecipChance}%, Total Precipitation: ${totalPrecipSum}mm<br>`;
   message += `Max Wind Speed: ${maxWind}km/h<br>`;
   message += `Minimum Cloud Cover: ${minCloudCover}%<br>`;
-
+  
   week.innerHTML = message;
 }
 
 function makeWeatherChoices(maxTemperature, minTemperature, maxUVIndex, precipitationChance, windspeed, percipitationsum, cloudcover, date) {
   let message = `<strong>${date}</strong><br>`;
   message += `According to the conditions, the following is recommended:<br>`;
+  images.innerHTML=`<p></p>`
 
-  if (maxTemperature > 20) message += `<br>It will be warm outside. Remember to stay hydrated.`;
-  if (maxTemperature > 40) message += `<br>It will be very hot. Plan accordingly.`;
-  else if (maxTemperature > 30) message += `<br>It will be quite hot. Consider dressing lightly.`;
-  else if (maxTemperature > 25) message += `<br>It will be hot. Consider wearing something fresh.`;
+  if (maxTemperature > 40) {
+    message += `<br>It will be very hot. Plan accordingly.`;
+  }  else if (maxTemperature > 30) {
+    message += `<br>It will be quite hot. Consider dressing lightly.`;
+  }  else if (maxTemperature > 25) {
+    message += `<br>It will be hot. Consider wearing something fresh.`;
+  } else if (maxTemperature > 20) {
+    message += `<br>It will be warm outside. Remember to stay hydrated.`;
+    images.innerHTML += `<img src="bilder/waterbottle.jpg" height="150">`
+  }
 
-  if (minTemperature < -10) message += `<br>It could be quite cold. Remember to layer up.`;
-  else if (minTemperature < 0) message += `<br>Temperatures could go below 0°C. Consider bringing gloves or a scarf.`;
-  else if (minTemperature < 10) message += `<br>It could be cold. Consider bringing a jacket.`;
-  else if (minTemperature < 15) message += `<br>It could be a bit fresh. Consider bringing a pullover.`;
+  if (minTemperature < -10) {
+    message += `<br>It could be quite cold. Remember to layer up.`;
+  }  else if (minTemperature < 0) {
+    message += `<br>Temperatures could go below 0°C. Consider bringing gloves or a scarf.`;
+    images.innerHTML += `<img src="bilder/gloves.jpg" height="150">`
+    images.innerHTML += `<img src="bilder/scarf.png" height="150">`
+  }  else if (minTemperature < 10) {
+    message += `<br>It could be cold. Consider bringing a jacket.`;
+    images.innerHTML += `<img src="bilder/jacket.jpg" height="150">`
+  } else if (minTemperature < 15) {
+    message += `<br>It could be a bit fresh. Consider bringing a pullover.`;
+    images.innerHTML += `<img src="bilder/jumper.jpg" height="150">`
+  }
 
-  if (maxUVIndex > 4) message += `<br>It is imperative you wear sunscreen.`;
-  else if (maxUVIndex > 3) message += `<br>It is highly recommended you wear sunscreen.`;
-  else if (maxUVIndex > 2) message += `<br>Remember to wear sunscreen.`;
+  if (maxUVIndex > 4) {
+    message += `<br>It is imperative you wear sunscreen.`;
+    images.innerHTML += `<img src="bilder/sunscreen.png" height="150">`
+  }  else if (maxUVIndex > 3) {
+    message += `<br>It is highly recommended you wear sunscreen.`;
+    images.innerHTML += `<img src="bilder/sunscreen.png" height="150">`
+  }  else if (maxUVIndex > 2) {
+    message += `<br>Remember to wear sunscreen.`;
+  }
 
-  if (precipitationChance > 25 && windspeed > 10)
+  if (precipitationChance > 25 && windspeed > 10) {
     message += `<br>There is a good chance of precipitation and it might get windy. Bring a raincoat.`;
-  else if (precipitationChance > 25)
+    images.innerHTML += `<img src="bilder/raincoat.jpg" height="150">`
+  }  else if (precipitationChance > 25){
     message += `<br>There is a good chance of precipitation. Bring an umbrella or a raincoat.`;
+    images.innerHTML += `<img src="bilder/raincoat.jpg" height="150">`
+    images.innerHTML += `<img src="bilder/umbrella.png" height="150">`
+  }
 
-  if (cloudcover < 30) message += `<br>It could get quite bright. Bring sunglasses or a cap.`;
-
-  message += `<br><br>Max Temp: ${maxTemperature}°C, Min Temp: ${minTemperature}°C<br>`;
+  if (cloudcover < 30) {
+    message += `<br>It could get quite bright. Bring sunglasses or a cap.`;
+    images.innerHTML += `<img src="bilder/sunglasses.png" height="150">`
+    images.innerHTML += `<img src="bilder/cap.jpg" height="150">`
+  }
+  
+  message += `<br><br><strong>Daily Extremes:</strong><br>`
+  message += `Max Temp: ${maxTemperature}°C, Min Temp: ${minTemperature}°C<br>`;
   message += `Max UV Index: ${maxUVIndex}<br>`;
   message += `Precipitation Chance: ${precipitationChance}%, Sum: ${percipitationsum}mm<br>`;
   message += `Max Wind Speed: ${windspeed}km/h<br>`;
